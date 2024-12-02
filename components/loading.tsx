56873cfe4abba6dbf330aae69a17ad35268ac175
hemanth <applicationcomputer257@gmail.com>
2024-12-02 10:54:05 +0530

    loading state chanfe

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,13 +2,10 @@ import React from "react";
 
 export const SandboxLoading = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black ">
+    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden max-h-screen w-screen bg-black ">
       {/* Logo Container */}
-      <div className="mb-8 animate-fade-in-down text-[#C1C4D4]">Sandbox</div>
-
-      {/* Loading Bar Container */}
-      <div className="w-48 h-1 bg-gray-200 rounded-full overflow-hidden">
-        <div className="w-full h-full bg-blue-800 animate-loading-bar" />
+      <div className="mb-8 animate-fade-in-down text-primary text-[20rem]">
+        SANDBOX
       </div>
     </div>
   );
